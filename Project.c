f1824ae8828f5fd0d59d0b1a0085be2dc2edc1b0
davidtheory <dgf2001@hw.ac.uk>
2024-01-26 16:37:40 +0400

    Preparing the project

--- Project.c
+++ Project.c
@@ -1,1 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <openssl/aes.h>
+
+void fileEncrypt(){} // Encryption method
+
+void fileDecrypt(){} //Decryption method
+
+int main(int argc, char *argv[]){}
