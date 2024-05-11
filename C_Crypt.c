b8ea3667774caf20e824607f26310dacf698387f
davidtheory <dgf2001@hw.ac.uk>
2024-05-11 16:47:07 +0400

    Changed file name

---
+++ C_Crypt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <openssl/aes.h>
+
+/* TODO
+ *
+ * 
+ * 
+ * 
+*/
+void fileEncrypt(){} // Encryption method
+
+void fileDecrypt(){} //Decryption method
+
+int main(int argc, char *argv[]){}
